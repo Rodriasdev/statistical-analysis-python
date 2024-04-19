b2cb0e8f214e4083333e3b045703f5b62c99fc61
Rodriasd <veronrodrigo98@gmail.com>
2024-04-18 23:42:30 -0300

    was added the error handling

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 import pandas as pd
 
 edades = [19,29,19,22,23,19,30,19,19,19,20,20,20,18,22,19,34,34,21,21,22,28,29,19
-        ,20,19,25,28,21,22]
+        ,20,19,25,28,21,22, "a"]
 
 def analisis_estadistico(lista): 
-    data = pd.Series(lista)
-    df = pd.DataFrame({"edades": dict(data.value_counts()).keys(), "fi": dict(data.value_counts()).values()})
+    try:
+        if not isinstance(lista, list):
+            raise TypeError("El valor de entrada debe ser una lista.")
+        
+        if len(lista) == 0:
+            print("La lista no debe estar vacía.")
+            return
+        
+   
+        if not all(isinstance(x, (int, float)) for x in lista):
+            raise TypeError("Todos los elementos de la lista tienen que ser númericos.")
+        
+        data = pd.Series(lista)
+        df = pd.DataFrame({"edades": dict(data.value_counts()).keys(), "fi": dict(data.value_counts()).values()})
 
-    df["Fi"] = df['fi'].cumsum()
+        df["Fi"] = df['fi'].cumsum()
 
-    df["ri"] = (df["fi"] / df["fi"].sum()).round(4)
+        df["ri"] = (df["fi"] / df["fi"].sum()).round(4)
 
-    df["Ri"] = df['ri'].cumsum()
+        df["Ri"] = df['ri'].cumsum()
 
-    df["pi%"] = df['ri'] * 100
+        df["pi%"] = df['ri'] * 100
 
-    df["Pi%"] = df['Ri'] * 100
+        df["Pi%"] = df['Ri'] * 100
 
-    print(df)
+        print(df)
+    except TypeError as e:
+        print('Error:', e)
 
 analisis_estadistico(edades)
